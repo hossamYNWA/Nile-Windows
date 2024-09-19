18e773a98cd93a3786ab02f5d8b024a72095d66b
Hossam Mousa <hossam.mousa99@yahoo.com>
2024-09-19 07:46:59 +0300

    Delete backend directory
    deleted backend directory

--- backend/config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const DBConnection = () => {
-    mongoose.connect(process.env.DB_URI).then((conn) => {
-        console.log(`connection ${conn.connection.host}`)
-    })
-}
-
-module.exports = DBConnection
--- backend/models/TypeOfUnitModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const TypeOfUnit = new mongoose.Schema({
-    type: {
-        type: String,
-    },
-}, { timeStamp: true })
-
-module.exports = mongoose.model("TypeOfUnit", TypeOfUnit)
